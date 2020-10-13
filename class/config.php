bb0c51008b6728baeb294515e589b3aa08c09fd9
Rafael da Silva Souza <silvasouza.rafael@eb.mil.br>
2020-10-13 17:02:41 -0300

    feat: criaçao da tela de login

--- class/config.php
+++ class/config.php
@@ -2,6 +2,6 @@
 define('DEBUG', TRUE);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'she');
-define('DB_USER', 'rafael');
+define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_ENCODING', 'utf8');
