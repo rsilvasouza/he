8e1a97114818fed04e56d45b80f462ff273aa1a1
Mayara <mayagv_silva@yahoo.com.br>
2020-12-01 21:30:15 -0300

    Acertando o nome do formulário de edição de atividade do aluno.

--- alunoAtividade.php
+++ alunoAtividade.php
@@ -12,7 +12,8 @@ $atividades = new Atividade();
             <h1>Atividades Cadastradas</h1>
         </div>
         <div class="form-group col-md-2">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cadastrar">Cadastrar</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal"
+                data-target="#cadastrar">Cadastrar</button>
         </div>
     </div>
     <table id="listar" class="display" style="width:100%">
@@ -45,22 +46,26 @@ $atividades = new Atividade();
 
             
         ?>
-            <tr>
-                <td><?php echo $value->descricao; ?></td>
-                <td><?php echo $value->nome; ?></td>
-                <td class="text-center"><?php echo $periodo; ?> </td>
-                <td class="text-center"><?php echo $value->carga_horaria; ?></td>
-                <td class="text-center"><?php echo $alunoAtividade->situacao($value->status) . $motivo; ?></td>
-                <td class="text-center">
-                    <?php echo $arquivo; ?>
-                </td>
-                <td class="text-center">
-                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editar" onclick="preencheDados('editar', <?php echo '\'' . $value->id . '\',' . '\'' . $value->descricao . '\',' . '\'' . $value->atividade_id . '\',' . '\'' . $value->data_inicial . '\',' . '\'' . $value->data_final . '\',' . '\'' . $value->hora_inicial . '\',' . '\'' . $value->hora_final . '\',' . '\'' . $value->carga_horaria . '\',' . '\'' . $value->observacao . '\'' ?>)" <?php echo $disable; ?>>Editar</button>
-                </td>
-                <td class="text-center">
-                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#excluir" onclick="preencheDados('excluir', <?php echo $value->id; ?>)" <?php echo $disable; ?>>Excluir</button>
-                </td>
-            </tr>
+        <tr>
+            <td><?php echo $value->descricao; ?></td>
+            <td><?php echo $value->nome; ?></td>
+            <td class="text-center"><?php echo $periodo; ?> </td>
+            <td class="text-center"><?php echo $value->carga_horaria; ?></td>
+            <td class="text-center"><?php echo $alunoAtividade->situacao($value->status) . $motivo; ?></td>
+            <td class="text-center">
+                <?php echo $arquivo; ?>
+            </td>
+            <td class="text-center">
+                <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#editar"
+                    onclick="preencheDados('editar', <?php echo '\'' . $value->id . '\',' . '\'' . $value->descricao . '\',' . '\'' . $value->atividade_id . '\',' . '\'' . $value->data_inicial . '\',' . '\'' . $value->data_final . '\',' . '\'' . $value->hora_inicial . '\',' . '\'' . $value->hora_final . '\',' . '\'' . $value->carga_horaria . '\',' . '\'' . $value->observacao . '\'' ?>)"
+                    <?php echo $disable; ?>>Editar</button>
+            </td>
+            <td class="text-center">
+                <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#excluir"
+                    onclick="preencheDados('excluir', <?php echo $value->id; ?>)"
+                    <?php echo $disable; ?>>Excluir</button>
+            </td>
+        </tr>
 
 
 
@@ -95,7 +100,7 @@ $atividades = new Atividade();
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Edição de Curso</h5>
+                <h5 class="modal-title">Edição de Atividade</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -107,7 +112,8 @@ $atividades = new Atividade();
                     <div class="form-row">
                         <div class="form-group col-md-12">
                             <label>Nome da Atividade</label>
-                            <input type="text" name="descricao" id="descricao" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="descricao" id="descricao" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
 
@@ -117,7 +123,9 @@ $atividades = new Atividade();
                             <select class="form-control" name="atividade" id="atividade">
                                 <option value="">Selecione</option>
                                 <?php foreach ($atividades->findAll() as $key => $atividade) : ?>
-                                    <option value="<?php echo $atividade->id; ?>" <?php echo ($atividade->id == $value->atividade_id) ? 'selected' : ''; ?>><?php echo $atividade->nome; ?></option>
+                                <option value="<?php echo $atividade->id; ?>"
+                                    <?php echo ($atividade->id == $value->atividade_id) ? 'selected' : ''; ?>>
+                                    <?php echo $atividade->nome; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
@@ -126,43 +134,50 @@ $atividades = new Atividade();
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Período da Atividade</label>
-                            <input type="date" name="dataInicial" id="dataInicial" class="form-control" aria-describedby="helpId">
+                            <input type="date" name="dataInicial" id="dataInicial" class="form-control"
+                                aria-describedby="helpId">
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>&nbsp;</label>
-                            <input type="date" name="dataFinal" id="dataFinal" class="form-control" aria-describedby="helpId">
+                            <input type="date" name="dataFinal" id="dataFinal" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Horário da Atividade</label>
-                            <input type="text" name="horaInicial" id="horaInicial" onfocus="formataHora()" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="horaInicial" id="horaInicial" onfocus="formataHora()"
+                                class="form-control" aria-describedby="helpId">
                             <small id="emailHelp" class="form-text text-muted">Período ou hora da atividade.</small>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>&nbsp;</label>
-                            <input type="text" name="horaFinal" id="horaFinal" onfocus="formataHora()" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="horaFinal" id="horaFinal" onfocus="formataHora()"
+                                class="form-control" aria-describedby="helpId">
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Carga Horária</label>
-                            <input type="text" name="cargaHoraria" id="cargaHoraria" onfocus="formataHora()" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="cargaHoraria" id="cargaHoraria" onfocus="formataHora()"
+                                class="form-control" aria-describedby="helpId">
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>Anexar Arquivo</label>
-                            <input type="file" name="arquivo" id="arquivo" class="form-control-file" aria-describedby="helpId">
+                            <input type="file" name="arquivo" id="arquivo" class="form-control-file"
+                                aria-describedby="helpId">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-12">
                             <label>Observação</label>
-                            <input type="text" name="observacao" id="observacao" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="observacao" id="observacao" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
                 </div>
@@ -191,7 +206,8 @@ $atividades = new Atividade();
                     <div class="form-row">
                         <div class="form-group col-md-12">
                             <label>Nome da Atividade</label>
-                            <input type="text" name="descricao" id="descricao" class="form-control" aria-describedby="helpId" required>
+                            <input type="text" name="descricao" id="descricao" class="form-control"
+                                aria-describedby="helpId" required>
                         </div>
                     </div>
 
@@ -201,7 +217,7 @@ $atividades = new Atividade();
                             <select class="form-control" name="atividade" id="atividade" required>
                                 <option value="">Selecione</option>
                                 <?php foreach ($atividades->findAll() as $key => $atividade) : ?>
-                                    <option value="<?php echo $atividade->id; ?>"><?php echo $atividade->nome; ?></option>
+                                <option value="<?php echo $atividade->id; ?>"><?php echo $atividade->nome; ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
@@ -210,44 +226,51 @@ $atividades = new Atividade();
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Período da Atividade</label>
-                            <input type="date" name="dataInicial" id="dataInicial" class="form-control" aria-describedby="helpId" required>
+                            <input type="date" name="dataInicial" id="dataInicial" class="form-control"
+                                aria-describedby="helpId" required>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>&nbsp;</label>
-                            <input type="date" name="dataFinal" id="dataFinal" class="form-control" aria-describedby="helpId">
+                            <input type="date" name="dataFinal" id="dataFinal" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Horário da Atividade</label>
-                            <input type="text" name="horaInicial" id="horaInicial2" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="horaInicial" id="horaInicial2" class="form-control"
+                                aria-describedby="helpId">
                             <small id="emailHelp" class="form-text text-muted">Período ou hora da atividade.</small>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>&nbsp;</label>
-                            <input type="text" name="horaFinal" id="horaFinal2" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="horaFinal" id="horaFinal2" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label>Carga Horária</label>
-                            <input type="text" name="cargaHoraria" id="cargaHoraria2" class="form-control" aria-describedby="helpId" required>
+                            <input type="text" name="cargaHoraria" id="cargaHoraria2" class="form-control"
+                                aria-describedby="helpId" required>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label>Anexar Arquivo</label>
-                            <input type="file" name="arquivo" id="arquivo" class="form-control-file" aria-describedby="helpId" required>
+                            <input type="file" name="arquivo" id="arquivo" class="form-control-file"
+                                aria-describedby="helpId" required>
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="form-group col-md-12">
                             <label>Observação</label>
-                            <input type="text" name="observacao" id="observacao" class="form-control" aria-describedby="helpId">
+                            <input type="text" name="observacao" id="observacao" class="form-control"
+                                aria-describedby="helpId">
                         </div>
                     </div>
                 </div>
@@ -263,40 +286,41 @@ $atividades = new Atividade();
 <?php require_once 'include/rodape.php' ?>
 
 <script>
-    $(document).ready(function() {
-        $('#listar').dataTable({
-            searching: true,
-            responsive: true
-        });
+$(document).ready(function() {
+    $('#listar').dataTable({
+        searching: true,
+        responsive: true
     });
+});
 
-    function preencheDados(tipo, id, descricao, atividade, dataInicial, dataFinal, horaInicial, horaFinal, cargaHoraria, observacao) {
-        if (tipo == 'editar') {
-            $('#id').val(id);
-            $('#descricao').val(descricao);
-            $('#atividade').val(atividade);
-            $('#dataInicial').val(dataInicial);
-            $('#dataFinal').val(dataFinal);
-            $('#horaInicial').val(horaInicial);
-            $('#horaFinal').val(horaFinal);
-            $('#cargaHoraria').val(cargaHoraria);
-            $('#observacao').val(observacao);
-        } else if (tipo == 'excluir') {
-            $('#idExcluir').val(id);
-        }
+function preencheDados(tipo, id, descricao, atividade, dataInicial, dataFinal, horaInicial, horaFinal, cargaHoraria,
+    observacao) {
+    if (tipo == 'editar') {
+        $('#id').val(id);
+        $('#descricao').val(descricao);
+        $('#atividade').val(atividade);
+        $('#dataInicial').val(dataInicial);
+        $('#dataFinal').val(dataFinal);
+        $('#horaInicial').val(horaInicial);
+        $('#horaFinal').val(horaFinal);
+        $('#cargaHoraria').val(cargaHoraria);
+        $('#observacao').val(observacao);
+    } else if (tipo == 'excluir') {
+        $('#idExcluir').val(id);
     }
+}
 
 
 
-    // function validaPeriodo(dataInicial, dataFinal){
-    //     if(dataFinal < dataInicial){
-    //         document.getElementById("botao").disabled = true;
-    //         console.log(dataInicial);
-    //         console.log(dataFinal);
-    //         return alert ('Período inválido!');
-    //     }else{
-    //         document.getElementById("botao").disabled = false;
-    //         return elemento.style.backgroundColor = "";
-    //     }
-    // }
+// function validaPeriodo(dataInicial, dataFinal){
+//     if(dataFinal < dataInicial){
+//         document.getElementById("botao").disabled = true;
+//         console.log(dataInicial);
+//         console.log(dataFinal);
+//         return alert ('Período inválido!');
+//     }else{
+//         document.getElementById("botao").disabled = false;
+//         return elemento.style.backgroundColor = "";
+//     }
+// }
 </script>
